a45d086896ab04a7a0250b868bf98fbe3ca7df01
Dmitriy <x-sad-x@ya.ru>
2019-06-06 17:40:02 +0700

    Updates

---
+++ precache-manifest.7254bdc37611973af7a4232dbd1456c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b201e3407083d5fcbdd47ea17c6e10cf",
+    "url": "/gitapi/index.html"
+  },
+  {
+    "revision": "8a9f30fc01c320b8b688",
+    "url": "/gitapi/static/css/main.09f9b19b.chunk.css"
+  },
+  {
+    "revision": "d3e97e00eadbf8bf2ab5",
+    "url": "/gitapi/static/js/2.bf876ea5.chunk.js"
+  },
+  {
+    "revision": "8a9f30fc01c320b8b688",
+    "url": "/gitapi/static/js/main.a9ae09c5.chunk.js"
+  },
+  {
+    "revision": "0562dfaecf7535d44dcb",
+    "url": "/gitapi/static/js/runtime~main.ff5c9c98.js"
+  }
+]);
